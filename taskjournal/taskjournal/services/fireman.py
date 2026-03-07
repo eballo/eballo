724dbb11dbe14bac02a8713ba6ab37bb63e7a48d
Enric <enric.ballo@gmail.com>
2026-03-07 23:03:45 +0100

    #75 Small improvements test (#76)

--- taskjournal/taskjournal/services/fireman.py
+++ taskjournal/taskjournal/services/fireman.py
@@ -7,19 +7,19 @@ from taskjournal.services.logger import logger
 
 
 class FiremanService:
-    def __init__(self, create_datetime: datetime, debug: bool = False) -> None:
-        self.debug = debug
+    def __init__(self, create_datetime: datetime) -> None:
         self.fireman_weeks = set()
-        self.load_and_parse(create_datetime)
+        self.create_datetime = create_datetime
+        self.load_and_parse()
 
     @staticmethod
     def _get_monday(date_obj: date) -> date:
         """Normalizes any date to the Monday of that week"""
         return date_obj - timedelta(days=date_obj.weekday())
 
-    def load_and_parse(self, create_datetime: datetime) -> None:
+    def load_and_parse(self) -> None:
         try:
-            year = create_datetime.year
+            year = self.create_datetime.date().year
             fireman_path = os.path.join(BASE_DIR, f"{year}/{FIREMAN_WEEKS_FILE}")
             logger.debug(f"Loading fireman weeks from {fireman_path}")
             with open(fireman_path, "r", encoding="utf-8") as f:
@@ -37,14 +37,15 @@ class FiremanService:
                             monday_date = self._get_monday(raw_date)
                             self.fireman_weeks.add(monday_date)
                         except ValueError:
-                            if self.debug:
-                                print(f"Invalid date: {line}")
+                            logger.error(f"Invalid date: {line}")
         except FileNotFoundError:
             logger.error(f"Error: File '{fireman_path}' not found.")
 
-    def is_fireman_week(self, date_obj: date) -> bool:
+    def is_fireman_week(self) -> bool:
         """Returns True if the date's week is in the fireman list"""
-        normalized_input = self._get_monday(date_obj)
+        normalized_input = self._get_monday(self.create_datetime.date())
         is_fireman_week = normalized_input in self.fireman_weeks
-        logger.info(f"Checking if {date_obj} is a fireman week: {is_fireman_week}")
+        logger.info(
+            f"Checking if {self.create_datetime.date()} is a fireman week: {is_fireman_week}"
+        )
         return is_fireman_week
